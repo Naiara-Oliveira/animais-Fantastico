2b2bdd387218e6bd2ee8c01970ad32d591b790dd
Naiara Oliveira <nayaraoliveira.9722@gmail.com>
2022-02-18 17:06:12 -0400

    posicao scroll animacao

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 import initScrollSuave from './modules/scroll-suave.js';
-import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tab-nav.js';
 import initModal from './modules/modal.js';
@@ -9,9 +8,9 @@ import initmenuMobile from './modules/menu-mobile.js';
 import initFuncionamneto from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initBitcoin from './modules/fetch-bitcoin.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
 
 initScrollSuave();
-initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
@@ -21,3 +20,4 @@ initmenuMobile();
 initFuncionamneto();
 initFetchAnimais();
 initBitcoin();
+initAnimacaoScroll();
