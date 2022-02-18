d340cd26e64f931078040af0611755f0ba4022a6
Naiara Oliveira <nayaraoliveira.9722@gmail.com>
2022-02-18 17:57:28 -0400

    Accordion transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-import initScrollSuave from './modules/scroll-suave.js';
-import initAccordion from './modules/accordion.js';
+import ScrollSuave from './modules/scroll-suave.js';
+import Accordion from './modules/accordion.js';
 import initTabNav from './modules/tab-nav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
@@ -10,8 +10,12 @@ import initFetchAnimais from './modules/fetch-animais.js';
 import initBitcoin from './modules/fetch-bitcoin.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 
-initScrollSuave();
-initAccordion();
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
+
 initTabNav();
 initModal();
 initTooltip();
